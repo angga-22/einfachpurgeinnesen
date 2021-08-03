ba658f2e7a7b2165f806f18591effab5ddeb5df9
angga-22 <anggasp.unib@gmail.com>
2021-08-03 09:39:04 +0700

    develop header and all the stuff

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Section from '../Box'
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { StaticImage } from 'gatsby-plugin-image'
+
+import Section from "../Box";
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx } from "theme-ui";
 const Card = () => {
-
-
   const data = useStaticQuery(graphql`
     query MyQuery {
       allPurDataJson {
@@ -21,40 +21,80 @@ const Card = () => {
         }
       }
     }
-  `)
-  console.log(data.allPurDataJson.edges)
+  `);
 
   return (
     <div>
-      {data.allPurDataJson.edges.map(item => {
+      {data.allPurDataJson.edges.map((item, i) => {
         return (
           <Section>
             <h1
               sx={{
-                fontFamily: 'label',
-                fontSize: [2, 3, 4, 5, 6, 7]
+                fontFamily: "label",
+                fontSize: [2, 3, 4, 5, 6, 7],
               }}
-            >{item.node.label} <span style={{ color: 'red' }}>{item.node.type === "vegan" ? "vegan" : item.node.type === "vegetarian" ? "vegetarian" : null}</span> </h1>
+            >
+              {item.node.label}{" "}
+              <span >
+                {item.node.type === "vegan"
+                  ? <StaticImage
+                    alt="vegan"
+                    width="20"
+                    src={'../../images/vegan.png'}
+                  />
+                  : item.node.type === "vegetarian"
+                    ? <StaticImage
+                      alt="vegan"
+                      width="20"
+                      src={'../../images/vegetarian.png'}
+                    />
+                    : null}
+              </span>
+            </h1>
             <p
               sx={{
-                fontFamily: 'body'
+                fontFamily: "body",
               }}
-            >{item.node.description}</p>
+            >
+              {i === 0 ?
+
+                item.node.description.slice(0, 80) + " " +
+                item.node.description.slice(80, 99) + " " +
+                item.node.description.slice(99)
+                :
+                item.node.description
+              }
+
+            </p>
             <p
               sx={{
-                fontFamily: 'body'
+                fontFamily: "body",
               }}
-            >{item.node.currentPrice}</p>
+            >
+              <StaticImage
+                alt="vegan"
+                width="20"
+                src={'../../images/bullet-small.png'}
+              />
+              {item.node.currentPrice}
+            </p>
             <p
               sx={{
-                fontFamily: 'body'
+                fontFamily: "body",
               }}
-            >{item.node.nextPrice}</p>
+            >
+              <StaticImage
+                alt="vegan"
+                width="20"
+                src={'../../images/bullet-big.png'}
+              />
+              {item.node.nextPrice}
+            </p>
           </Section>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,79 @@
 import * as React from 'react'
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-
+import { Flex } from 'theme-ui'
+import Section from '../Box'
+import { StaticImage } from 'gatsby-plugin-image'
+import { useStaticQuery, graphql } from 'gatsby'
 const Header = () => {
+
+
+  const datas = useStaticQuery(graphql`
+    query tes {
+      allPurHoursJson {
+        edges {
+          node {
+            id
+            label
+            hours
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <>
       <header>
-        <h1>Bistrokuche?!</h1>
+        <Flex as="nav"
+          sx={{
+            justifyContent: "center",
+            flexDirection: "column",
+            boxShadow: "4px 4px 4px black"
+          }}
+        >
+          <Section>
+            <h1>Bistrokuche?!</h1>
+            {datas.allPurHoursJson.edges.map(item => {
+              return (
+                <div>
+                  <h4>{item.node.label}</h4>
+                  <p>{item.node.hours}</p>
+                </div>
+              )
+            })}
+            <div
+              sx={{
+                display: 'flex',
+                flexDirection: "column"
+              }}
+            >
+              <div>
+                <StaticImage
+                  alt="vegan"
+                  width="20"
+                  src={'../../images/vegan.png'}
+                />
+                <span>Vegan oder vegane Option</span>
+              </div>
+              <div sx={{
+              }}>
+                <StaticImage
+                  width="12"
+                  alt="vegetarian"
+                  src={'../../images/vegetarian.png'}
+                />
+                <span>
+                  Vegatarisch</span>
+              </div>
+            </div>
+            <StaticImage
+              width="12"
+              alt="vegetarian"
+              src={'../../images/close.png'}
+            />
+          </Section>
+        </Flex>
       </header>
     </>
   )
