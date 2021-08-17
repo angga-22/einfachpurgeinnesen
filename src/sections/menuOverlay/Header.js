e363bbcc341d0c5e41ab13c0793848eba1ccc7db
angga-22 <anggasp.unib@gmail.com>
2021-08-17 11:18:34 +0700

    revamp header menu overlay to be grid layout

--- src/sections/menuOverlay/Header.js
+++ src/sections/menuOverlay/Header.js
@@ -2,7 +2,8 @@
 import { jsx, Flex, Box, Heading, Text } from "theme-ui"
 import { StaticImage } from 'gatsby-plugin-image'
 import { useStaticQuery, graphql } from 'gatsby'
-
+import { GridWrapper, GridItem } from '@thepuzzlers/pieces'
+import { Breakpoints } from '../../components/Breakpoints'
 const Header = ({ handleClose }) => {
   const data = useStaticQuery(graphql`
     query getOpeningHours {
@@ -17,151 +18,196 @@ const Header = ({ handleClose }) => {
   `)
 
   return (
-    <div
+    <GridWrapper
       sx={{
-        maxWidth: ["100%", "100%", "100%", "85%", "85%", "90%"],
-        marginX: [null, null, null, "auto", "auto", "auto"],
-        padding: [
-          "5vw 7.5vw",
-          "5vw 9.4vw",
-          "5vw 7.6vw",
-          "2vw 2.6vw",
-          "2vw 2vw",
-          "2vw 2vw",
-        ],
-        borderRadius: ["12px"],
-        boxShadow: [
-          "0px 12px 24px rgba(0, 0, 0, 0.15)"
-        ]
+        // idth: ["100%", "100%", "100%", "85%", "85%", "90%"],
+        '@media screen and (max-width: 750px)': {
+          backgroundColor: 'primary',
+          boxShadow: [
+            "0px 12px 24px rgba(0, 0, 0, 0.15)"
+          ]
+        }
       }}
     >
-      <Flex
+      <GridItem
         sx={{
-          display: 'flex',
-          flexDirection: "column",
-          position: 'relative'
-        }}
-      >
-        <Heading sx={{
-          mb: ["24px", "8px", "12px", "12px", "12px", "12px"],
-          fontSize: ["32px", "42px", "38px", "40px", "40px", "43px"],
-          lineHeight: "125%"
-        }}>Bistrokuche?!</Heading>
-        <Box
+          p: [
+            "36px 0",
+            "21px 0",
+            "16px 0 30px 0",
+            "26px 0 31px 0",
+            "26px 0",
+            "26px 0",
+          ],
+          // m: [
+          //   "0",
+          //   "0",
+          //   "0",
+          //   "49px 0 0 0",
+          //   "56px 0 0 0",
+          //   "41px 0 0 0",
+          // ],
+          gridColumn: [
+            "1 / span 12",
+            "1 / span 12",
+            "1 / span 24 ",
+            " 1 / span 24",
+            " 1 / span 24",
+            " 1 / span 24",
+          ],
+          backgroundColor: 'primary',
+          borderRadius: ["12px"],
+          boxShadow: [
+            "0px 12px 24px rgba(0, 0, 0, 0.15)",
+          ],
+          '@media screen and (max-width: 750px)': {
+            backgroundColor: 'none',
+            borderRadius: ["0"],
+            boxShadow: 'none'
+          }
+        }}>
+        <GridItem
           sx={{
-            display: 'flex',
-            flexDirection: ["column", "column", "column", "column", "row", "row"],
-            justifyContent: "space-between",
+
+            gridColumn: [
+              "1 / span 12",
+              "1 / span 12",
+              "1 / span 24 ",
+              " 2 / span 22",
+              " 2 / span 22",
+              " 4 / span 22",
+            ],
+            p: ["0", "0", "0", "0 1.9em", "0 2.8em", "0 3.8em"]
           }}
         >
-          <Box>
-            {data.allPurHoursJson.nodes.map(item => {
-              return (
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: ["column", "row", "row", "row", "row", "row"],
-                  }}
-                >
-                  <Text
-                    variant="subHeading"
-                    sx={{
-                      mr: "1em",
-                      lineHeight: "175%",
-                      fontSize: [2, 2, 2, 3, 3, 3,]
-                    }}
-                  >{item.label}</Text>
-                  <Text
-                    variant="subHeading"
-                    sx={{
-                      lineHeight: "175%",
-                      fontWeight: '400',
-                      fontSize: [2, 2, 2, 3, 3, 3,]
-
-                    }}
-                  >{item.hours}</Text>
-                </Box>
-              )
-            })}
-          </Box>
-          <Box
+          <Flex
             sx={{
               display: 'flex',
-              flexDirection: ["column", "row", "row", "row", "column", "row"],
-              pt: ["24px", "16px", "12px", "12px", null, null],
-              pr: [null, null, "10px", "10px", "5em", "7em"],
-              mt: [null, null, null, "13px", "-3em", "-3em"]
+              flexDirection: "column",
+              position: 'relative',
             }}
           >
-            <Box sx={{
-              display: 'flex',
-              flexDirection: "row",
-              alignItems: 'center',
-              pb: ["1em"]
-            }}>
-              <StaticImage
-                alt="vegan"
-                width="17"
-                sx={{
-                  mr: '10px'
-                }}
-                src={'../../images/vegan.png'}
-              />
-              <Text
-                variant="subHeading"
-                sx={{
-                  pr: "48px",
-                  fontSize: [2, 2, 2, 3, 3, 3,]
-                }}
-              >Vegan oder vegane Option</Text>
-            </Box>
+            <Heading sx={{
+              mb: ["24px", "8px", "12px", "12px", "12px", "12px"],
+              fontSize: ["32px", "42px", "38px", "40px", "40px", "43px"],
+              lineHeight: "125%"
+            }}>Bistrokuche?!</Heading>
             <Box
               sx={{
                 display: 'flex',
-                flexDirection: "row",
-                alignItems: 'center',
-                pb: ["1em"]
+                flexDirection: ["column", "column", "column", "column", "row", "row"],
+                justifyContent: "space-between",
+              }}
+            >
+              <Box>
+                {data.allPurHoursJson.nodes.map(item => {
+                  return (
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: ["column", "row", "row", "row", "row", "row"],
+                      }}
+                    >
+                      <Text
+                        variant="subHeading"
+                        sx={{
+                          mr: "1em",
+                          lineHeight: "175%",
+                          fontSize: [2, 2, 2, 3, 3, 3,]
+                        }}
+                      >{item.label}</Text>
+                      <Text
+                        variant="subHeading"
+                        sx={{
+                          lineHeight: "175%",
+                          fontWeight: '400',
+                          fontSize: [2, 2, 2, 3, 3, 3,]
+
+                        }}
+                      >{item.hours}</Text>
+                    </Box>
+                  )
+                })}
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: ["column", "row", "row", "row", "column", "row"],
+                  pt: ["24px", "16px", "12px", "12px", null, null],
+                  pr: [null, null, "10px", "10px", "5em", "7em"],
+                  mt: [null, null, null, "13px", "-3em", "-3em"]
+                }}
+              >
+                <Box sx={{
+                  display: 'flex',
+                  flexDirection: "row",
+                  alignItems: 'center',
+                  pb: ["1em"]
+                }}>
+                  <StaticImage
+                    alt="vegan"
+                    width="17"
+                    sx={{
+                      mr: '10px'
+                    }}
+                    src={'../../images/vegan.png'}
+                  />
+                  <Text
+                    variant="subHeading"
+                    sx={{
+                      pr: "48px",
+                      fontSize: [2, 2, 2, 3, 3, 3,]
+                    }}
+                  >Vegan oder vegane Option</Text>
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: "row",
+                    alignItems: 'center',
+                    pb: ["1em"]
+                  }}
+                >
+                  <StaticImage
+                    width="10"
+                    alt="vegetarian"
+                    sx={{
+                      mr: '17px'
+                    }}
+                    src={'../../images/vegetarian.png'}
+                  />
+                  <Text variant="subHeading" sx={{
+                    fontSize: [2, 2, 2, 3, 3, 3,]
+                  }}>
+                    Vegatarisch</Text>
+                </Box>
+              </Box>
+            </Box>
+            <button
+              onClick={handleClose}
+              sx={{
+                top: [0, 0, 0, 0, "2em", "2.5em"],
+                right: [0],
+                position: "absolute",
+                cursor: "pointer",
+                // Reset of default button styles
+                border: 'none',
+                bg: 'unset'
               }}
             >
               <StaticImage
-                width="10"
-                alt="vegetarian"
                 sx={{
-                  mr: '17px'
+                  width: ["43px", "43px", "43px", "58px", "58px", "58px"]
                 }}
-                src={'../../images/vegetarian.png'}
+                alt="vegetarian"
+                src={'../../images/close.png'}
               />
-              <Text variant="subHeading" sx={{
-                fontSize: [2, 2, 2, 3, 3, 3,]
-              }}>
-                Vegatarisch</Text>
-            </Box>
-          </Box>
-        </Box>
-        <button
-          onClick={handleClose}
-          sx={{
-            top: [0, 0, 0, 0, "2em", "2.5em"],
-            right: [0],
-            position: "absolute",
-            cursor: "pointer",
-            // Reset of default button styles
-            border: 'none',
-            bg: 'unset'
-          }}
-        >
-          <StaticImage
-            sx={{
-              width: ["43px", "43px", "43px", "58px", "58px", "58px"]
-            }}
-            alt="vegetarian"
-            src={'../../images/close.png'}
-          />
-        </button>
+            </button>
 
-      </Flex>
-
-    </div>
+          </Flex>
+        </GridItem>
+      </GridItem>
+    </GridWrapper>
   )
 }
 
